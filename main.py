aa87a329c00ecdb8f0462dacfd820b582fc5ad2f
APBaluchon <aurelien-petitfrere@laposte.net>
2024-01-22 15:00:22 +0100

    up

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 import pytz
+import logging
 
-timezone_paris = pytz.timezone('Europe/Paris')
-timezone_reunion = pytz.timezone('Indian/Reunion')
+timezone = pytz.timezone('Europe/Paris')
 
-def get_date_formatted(timezone):
-    date = datetime.now(timezone)
-    return date.strftime("%H:%M:%S")
+logging.basicConfig(filename=None, encoding='utf-8', level=logging.DEBUG)
 
-print(get_date_formatted(timezone_paris))
-print(get_date_formatted(timezone_reunion))
+logging.info(f"Lancement du programme.")
+logging.debug(f"Demande d'heure sur le timezone : {timezone}")
+
+if timezone is None:
+
+    logging.error("aucune timezone n'a été renseignée")
+
+    raise ValueError("aucune timezone n'a été renseignée")
